dd5e5df60c70eada8fc8c24773e761b1046171f1
Les Aker <me@lesaker.org>
2017-02-14 13:57:24 -0800

    add headers and CSP

--- _assets/google/analytics.js
+++ _assets/google/analytics.js
@@ -8,7 +8,7 @@
     a.async = 1;
     a.src = g;
     m.parentNode.insertBefore(a, m)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-49086339-1', 'akerl.org');
 ga('send', 'pageview');
